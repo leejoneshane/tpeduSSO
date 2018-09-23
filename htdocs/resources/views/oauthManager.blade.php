e2f046496cda792f4e18af101d99ac83793d166d
leejoneshane <leejoneshane@gmail.com>
2018-09-23 21:32:02 +0800

    upload node-modules

--- htdocs/resources/views/oauthManager.blade.php
+++ htdocs/resources/views/oauthManager.blade.php
@@ -5,11 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8" style="margin-top: 20px">
         	@if (Auth::user()->is_admin)
-                <passport-clients></passport-clients>
-                @endif
-                <passport-authorized-clients></passport-authorized-clients>
-                <passport-personal-access-tokens></passport-personal-access-tokens>
-            </div>
+            <passport-clients></passport-clients>
+            @endif
+            <passport-authorized-clients></passport-authorized-clients>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
 	</div>
 </div>
--- htdocs/vendor/composer/autoload_real.php
+++ htdocs/vendor/composer/autoload_real.php
@@ -29,22 +29,13 @@ class ComposerAutoloaderInit68484b9b1f4c53072270d4986533a295
 
             call_user_func(\Composer\Autoload\ComposerStaticInit68484b9b1f4c53072270d4986533a295::getInitializer($loader));
         } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
+        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         if ($useStaticLoader) {
