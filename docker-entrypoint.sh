eae7247ea1de73c4ce82108b6f78834959b42910
Sean Lee <leejoneshane@gmail.com>
2018-09-23 21:52:06 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,6 @@ php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-composer dumpautoload --classmap-authoritative
 
 rm -f /run/apache2/httpd.pid
 exec httpd -DFOREGROUND
