3906350f5811e8f9dc566708d6f690dfe08746b8
Sean Lee <leejoneshane@gmail.com>
2018-09-24 12:08:57 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 set -euo pipefail
 chown -R apache:apache /var/www
-  
+composer update
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 if mysqlshow --host=${DB_HOST} --user=${DB_USERNAME} --password=${DB_PASSWORD} ${DB_DATABASE} users; then
   echo "database ready!"
 else
   php artisan migrate:refresh
   php artisan passport:install
 fi
-
-composer update
-php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
 rm -f /run/apache2/httpd.pid
 exec httpd -DFOREGROUND
