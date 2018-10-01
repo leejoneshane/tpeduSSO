4584dc8cf7c1a2eccdf8c868769fd207851aa591
leejoneshane <leejoneshane@gmail.com>
2018-10-01 11:59:53 +0800

    update sims connection test view

--- htdocs/resources/views/admin/synctest.blade.php
+++ htdocs/resources/views/admin/synctest.blade.php
@@ -18,35 +18,33 @@
 	    </div>
 	@endif
 	<div class="col-sm-12">
-			<div class="panel panel-default">	  
-			<div class="panel-heading">
-				<h4>scope: 學校資訊</h4>
+		<div class="panel panel-default">	  
+		<div class="panel-heading">
+			<h4>scope: 學校資訊</h4>
+		</div>
+		<div class="panel-body">
+			<div class="form-group custom-search-form">
+				<select id="field" name="field" class="form-control">
+					<option value="school_info" {{ $my_field == 'school_info' ? 'selected' : '' }}>學校基本資料</option>
+					<option value="department_info" {{ $my_field == 'department_info' ? 'selected' : '' }}>處室聯絡人資訊</option>
+					<option value="classes_info" {{ $my_field == 'classes_info' ? 'selected' : '' }}>班級資訊</option>
+					<option value="special_info" {{ $my_field == 'special_info' ? 'selected' : '' }}>特殊教育概況</option>
+				</select>
+				<label>學校統一編號：</label>
+				<input type="text" class="form-control" id="sid" name="sid" value="{{ old('sid') }}" required>
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button" onclick="location='{{ url()->current() }}?field=' + $('#field').val() + '&sid=' + $('#sid').val();">
+						傳送請求
+					</button>
+				</span>
 			</div>
-			<div class="panel-body">
-				<div class="input-group custom-search-form">
-					<select id="field" name="field" class="form-control" style="width: auto">
-						<option value="school_info" {{ $my_field == 'school_info' ? 'selected' : '' }}>學校基本資料</option>
-						<option value="department_info" {{ $my_field == 'department_info' ? 'selected' : '' }}>處室聯絡人資訊</option>
-						<option value="classes_info" {{ $my_field == 'classes_info' ? 'selected' : '' }}>班級資訊</option>
-						<option value="special_info" {{ $my_field == 'special_info' ? 'selected' : '' }}>特殊教育概況</option>
-					</select>
-					學校統一編號：<input type="text" class="form-control" style="width:auto" id="sid" name="sid" value="{{ old('sid') }}">
-					<span class="input-group-btn" style="width: auto">
-						<button class="btn btn-default" type="button" onclick="location='{{ url()->current() }}?field=' + $('#field').val() + '&sid=' + $('#sid').val();">
-							<i class="fa fa-eye"></i>
-						</button>
-					</span>
-				</div>
-			</div>
-			</div>
+		</div>
     	</div>
 	</div>
 	<div class="col-sm-12">
 		<div class="panel panel-default">
 		<div class="panel-heading">
-			<h4>
-				測試結果
-			</h4>
+			<h4>測試結果</h4>
 		</div>
 		<div class="panel-body">
 			<table class="table table-hover">
