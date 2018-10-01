605c45cf78e3550049bdab14193b418bfd90ebd7
leejoneshane <leejoneshane@gmail.com>
2018-10-01 16:32:06 +0800

    fix bug

--- htdocs/app/Http/Controllers/SyncController.php
+++ htdocs/app/Http/Controllers/SyncController.php
@@ -35,28 +35,56 @@ class SyncController extends Controller
     public function ps_testForm(Request $request)
     {
 		$my_field = $request->get('field');
+		$sid = $request->get('sid');
+		$grade = $request->get('grade');
+		$subjid = $request->get('subjid');
+		$clsid = $request->get('clsid');
+		$teaid = $request->get('teaid');
+		$stdno = $request->get('stdno');
+		$isbn = $request->get('isbn');
 		$http = new SimsServiceProvider();
 		$result = array();
-		if ($my_field && $request->has('sid')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid') ]);
-		}
-		if ($my_field && $request->has('sid') && $request->has('grade')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid'), '{grade}' => $request->get('grade') ]);
-		}
-		if ($my_field && $request->has('sid') && $request->has('subjid')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid'), '{subjid}' => $request->get('subjid') ]);
-		}
-		if ($my_field && $request->has('sid') && $request->has('clsid')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid'), '{clsid}' => $request->get('clsid') ]);
-		}
-		if ($my_field && $request->has('sid') && $request->has('teaid')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid'), '{teaid}' => $request->get('teaid') ]);
-		}
-		if ($my_field && $request->has('sid') && $request->has('stdno')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid'), '{stdno}' => $request->get('stdno') ]);
-		}
-		if ($my_field && $request->has('sid') && $request->has('isbn')) {
-			$result = $http->ps_call($my_field, [ '{sid}' => $request->get('sid'), '{isbn}' => $request->get('isbn') ]);
+		switch($my_field) {
+			case 'school_info':
+			case 'department_info':
+			case 'classes_info':
+			case 'special_info':
+			case 'calendar_info':
+			case 'library_books':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid ]);
+				break;
+			case 'classses_by_grade':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid, '{grade}' => $grade ]);
+				break;
+			case 'subject_info':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid, '{subjid}' => $subjid ]);
+				break;
+			case 'classs_info':
+			case 'classs_schedule':
+			case 'students_in_class':
+			case 'leaders_in_class':
+			case 'teachers_in_class':
+			case 'subject_for_class':
+			case 'class_lend_record':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid, '{clsid}' => $clsid ]);
+				break;
+			case 'teacher_info':
+			case 'teacher_schedule':
+			case 'teacher_tutor_students':
+			case 'subject_assign_to_teacher':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid, '{teaid}' => $teaid ]);
+				break;
+			case 'student_info':
+			case 'student_subjects_score':
+			case 'student_domains_score':
+			case 'student_attendance_record':
+			case 'student_health_record':
+			case 'student_parents_info':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid, '{stdno}' => $stdno ]);
+				break;
+			case 'book_info':
+				$result = $http->ps_call($my_field, [ '{sid}' => $sid, '{isbn}' => $isbn ]);
+				break;
 		}
 		return view('admin.synctest', [ 'my_field' => $my_field, 'result' => $result ]);
     }
