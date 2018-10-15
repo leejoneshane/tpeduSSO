d35e518e24c4471482b131cd8e58a65c5345de9c
leejoneshane <leejoneshane@gmail.com>
2018-10-15 14:26:07 +0800

    add debug messages

--- freeradius/docker-entrypoint.sh
+++ freeradius/docker-entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -e
 
-# prepare vpn
+echo prepare vpn
 mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
   mknod /dev/net/tun c 10 200
   chmod 0666 /dev/net/tun
 fi
 
+echo start openvpn client ....
 openvpn --script-security 2 --up /etc/openvpn/up.sh \
 	--status /etc/openvpn/client.status 10 --redirect-gateway def1 \
 	--cd /etc/openvpn --config client.conf
 
+echo restore default gateway ....
 sleep 5
 ip route del 0.0.0.0/1
 ip route del 128.0.0.0/1
 
+echo start freeradius ....
 radiusd -xx -f
