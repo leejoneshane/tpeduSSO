782e80620d68af400353bdf64d48dc29b7720627
leejoneshane <leejoneshane@gmail.com>
2018-11-04 16:50:12 +0800

    微調表單按鈕位置

--- htdocs/resources/views/admin/syncseatinfo.blade.php
+++ htdocs/resources/views/admin/syncseatinfo.blade.php
@@ -13,12 +13,10 @@
 			{{ $line }}<br>
 		@endforeach
 	@else
+		<p>將從校務行政系統將學生就讀班級座號同步到 openldap，同步過程中會自動將畢業學生標註為刪除，但無法新增學生和處理學籍異動（轉入或轉出）。</p>
+		<p>同步過程需要時間，直到結果出現為止，請勿關閉瀏覽器或離開此網頁，以避免同步程序被關閉。</p>			
 		<form role="form" method="POST">
 			<div class="form-group">
-				<label>
-					<p>將從校務行政系統將學生就讀班級座號同步到 openldap，同步過程中會自動將畢業學生標註為刪除，但無法新增學生和處理學籍異動（轉入或轉出）。</p>
-					<p>同步過程需要時間，直到結果出現為止，請勿關閉瀏覽器或離開此網頁，以避免同步程序被關閉。</p>			
-				</label>
 				<button class="btn btn-default" type="submit">
 					我瞭解了，請開始同步
 				</button>
