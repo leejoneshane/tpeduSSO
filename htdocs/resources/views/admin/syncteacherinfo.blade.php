026e065337b33ff1f8a3bb08216274b2860270fe
leejoneshane <leejoneshane@gmail.com>
2019-01-22 14:32:26 +0800

    patch bugs

--- htdocs/resources/views/admin/syncteacherinfo.blade.php
+++ htdocs/resources/views/admin/syncteacherinfo.blade.php
@@ -16,12 +16,7 @@
 	@else
 		<p>將從校務行政系統將教師資料同步到 openldap，同步過程中會自動修正教師個人資料、設定單位職稱、級任、配課資訊，並將離職教師標註為刪除，但無法處理帳號密碼同步。</p>
 		<p>同步過程需要時間，直到結果出現為止，請勿關閉瀏覽器或離開此網頁，以避免同步程序被關閉。</p>			
-		@if ($sims == 'alle')
-		<form role="form" method="POST" action="{{ route('school.ps.sync_teacher', [ 'dc' => $dc ]) }}">
-		@endif
-		@if ($sims == 'oneplus')
-		<form role="form" method="POST" action="{{ route('school.js.sync_teacher', [ 'dc' => $dc ]) }}">
-		@endif
+		<form role="form" method="POST" action="{{ route('school.sync_teacher', [ 'dc' => $dc ]) }}">
 			@csrf
 			<div class="form-group">
 				<button class="btn btn-default" type="submit" name="submit" value="true">
