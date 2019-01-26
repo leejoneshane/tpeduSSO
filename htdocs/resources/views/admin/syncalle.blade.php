91fe1587a044f6c529db71af71447b943de36901
leejoneshane <leejoneshane@gmail.com>
2019-01-26 11:13:06 +0800

    更新自動同步作業提示訊息

--- htdocs/resources/views/admin/syncalle.blade.php
+++ htdocs/resources/views/admin/syncalle.blade.php
@@ -14,7 +14,7 @@
 			@endforeach
 		</div>
 	@else
-		<p>即將進行自動化批次同步，同步作業將在背景執行，您可以離開頁面繼續操作，請使用 telescope 查看同步進度。</p>
+		<p>即將進行自動化批次同步，此同步作業將在背景執行，會自動同步所有標註為全誼校務行政系統之學校，您可以離開頁面繼續操作，請使用 telescope 查看同步進度。</p>
 		<form role="form" method="POST" action="{{ route('sync.ps.auto') }}">
 			@csrf
 			<div class="form-group">
