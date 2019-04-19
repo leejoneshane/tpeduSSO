2b266271bffab4e272893291bb7cea8cccbd1b22
李忠憲 <sean@lizhongxiandeMacBook-Air.local>
2019-04-19 11:33:02 +0800

    change function name

--- htdocs/app/Http/Controllers/Api_V2/v2_profileController.php
+++ htdocs/app/Http/Controllers/Api_V2/v2_profileController.php
@@ -11,9 +11,9 @@ use Illuminate\Http\Response;
 
 class v2_profileController extends Controller
 {
-	public function validate(Request $request)
+	public function valid_token(Request $request)
     {
-		return response()->json(['data' => 'Token is valid!' ]);
+		return response()->json(['data' => 'Token is valid!']);
 	}
 
     public function logout(Request $request)
