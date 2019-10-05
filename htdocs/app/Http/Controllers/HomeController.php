3173244a9a9f4e8d6d5d1fead37be355da9ed3d0
leejoneshane <leejoneshane@gmail.com>
2019-10-05 17:47:06 +0800

    fix bug

--- htdocs/app/Http/Controllers/HomeController.php
+++ htdocs/app/Http/Controllers/HomeController.php
@@ -68,7 +68,7 @@ class HomeController extends Controller
 		}
 		if (!$mobile) {
     		$userinfo['mobile'] = array();
-    		$user->mobile = NULL;
+    		$user->mobile = null;
 		}
 		$user->save();
 		$entry = $openldap->getUserEntry($idno);
