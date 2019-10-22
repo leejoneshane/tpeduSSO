66b2a178d5d67efaaa86566044b1485c4bc973f2
LEOSYS0827 <ldapsso0827@gmail.com>
2019-10-22 22:08:16 +0800

    上方標題字體調整

--- htdocs/resources/views/admin/schoolteacheredit.blade.php
+++ htdocs/resources/views/admin/schoolteacheredit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('page_heading')
-{{ isset($user) ? '編輯' : '新增' }}教師資訊
+<h1 class="page-header">{{ isset($user) ? '編輯' : '新增' }}教師資訊</h1>
 @endsection
 
 @section('section')
