54b0f9328dd6462c6897a8a0c4520e853fc891c1
LEOSYS0827 <ldapsso0827@gmail.com>
2019-10-22 21:58:18 +0800

    功能選單、頁面佈局及css樣式的調整

--- htdocs/resources/views/home.blade.php
+++ htdocs/resources/views/home.blade.php
@@ -1,19 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.userboard')
 
-@section('content')
+@section('page_heading')
+<h1 class="page-header">主控面板</h1>
+@endsection
+
+@section('section')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card card-default" style="margin-top: 20px">
-                <div class="card-header">主控面板</div>
-
+                <!--<div class="card-header">主控面板</div>-->
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     您已經登入系統！
                 </div>
             </div>
--- htdocs/resources/views/layouts/superboard.blade.php
+++ htdocs/resources/views/layouts/superboard.blade.php
@@ -1,56 +1,53 @@
 @extends('layouts.admin')
 
 @section('content')
-		<div id="wrapper">
-           <nav class="navbar-light sidebar" style="margin-top: 0px" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li {{ (Request::is('bureau/admin') ? 'class="active"' : '') }}>
-                            <a href="{{ route('bureau.admin') }}"><i class="fa fa-user-md fa-fw"></i>設定局端管理員</a>
-                        </li>
-                        <li {{ (Request::is('bureau/organization') ? 'class="active"' : '') }}>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i>教育機構管理</a>
-                            <ul class="nav nav-second-level">
-                                <li {{ (Request::is('bureau/organization') ? 'class="active"' : '') }}>
-                                    <a href="{{ route('bureau.organization') }}">線上編輯</a>
-                                </li>
-                                <li {{ (Request::is('bureau/organization/json') ? 'class="active"' : '') }}>
-                                    <a href="{{ route('bureau.jsonOrg') }}">匯入JSON</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li {{ (Request::is('bureau/group') ? 'class="active"' : '') }}>
-                            <a href="{{ route('bureau.group') }}"><i class="fa fa-group fa-fw"></i>動態群組管理</a>
-                        </li>
-                        <li {{ (Request::is('bureau/people') ? 'class="active"' : '') }}>
-                            <a href="#"><i class="fa fa-user fa-fw"></i>人員管理<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li {{ (Request::is('bureau/people') ? 'class="active"' : '') }}>
-                                    <a href="{{ route('bureau.people') }}">瀏覽及搜尋</a>
-                                </li>
-                                <li {{ (Request::is('bureau/people/new') ? 'class="active"' : '') }}>
-                                    <a href="{{ route('bureau.createPeople') }}">新增人員</a>
-                                </li>
-                                <li {{ (Request::is('bureau/people/json') ? 'class="active"' : '') }}>
-                                    <a href="{{ route('bureau.jsonPeople') }}">匯入JSON</a>
-                                </li>
-                            </ul>
-                        </li>
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3 style="font-size: 22px;text-align: center;font-weight: bold;">臺北市教育人員<br/>單一身份驗證服務</h3>
+            </div>
+            <ul class="list-unstyled components">
+				<li {{ (Request::is('bureau/admin') ? 'class=active' : '') }}>
+                    <a href="{{ route('bureau.admin') }}"><i class="fa fa-user-md fa-fw"></i>設定局端管理員</a>
+                </li>
+                <li>
+                    <a href="#submenu1" data-toggle="collapse" {{ (Request::is('bureau/organization','bureau/organization/*') ? 'aria-expanded=true' : 'aria-expanded=false') }} class="dropdown-toggle">
+						<i class="fa fa-sitemap fa-fw"></i>教育機構管理
+					</a>
+					@if(Request::is('bureau/organization','bureau/organization/*'))
+					<ul class="list-unstyled collapse in" id="submenu1" aria-expanded="true">
+					@else
+                    <ul class="list-unstyled collapse" id="submenu1">
+					@endif
+                        <li {{ (Request::is('bureau/organization') ? 'class=active' : '') }}><a class="func-item" href="{{ route('bureau.organization') }}">線上編輯</a></li>
+                        <li {{ (Request::is('bureau/organization/json') ? 'class=active' : '') }}><a class="func-item" href="{{ route('bureau.jsonOrg') }}">匯入JSON</a></li>
                     </ul>
-                </div>
-           	</nav>
-
-
-        	<div id="page-wrapper">
-				<div class="row">
-                	<div class="col-lg-12">
-                    	<h1 class="page-header">@yield('page_heading')</h1>
-                	</div>
-           		</div>
-				<div class="row">  
-					@yield('section')
-            	</div>
-        	</div>
-        </div>
+                </li>
+                <li {{ (Request::is('bureau/group','bureau/group/*') ? 'class=active' : '') }}>
+                    <a href="{{ route('bureau.group') }}"><i class="fa fa-users" aria-hidden="true"></i>動態群組管理</a>
+                </li>
+                <li>
+                    <a href="#submenu2" data-toggle="collapse" {{ (Request::is('bureau/people','bureau/people/*') ? 'aria-expanded=true' : 'aria-expanded=false') }} class="dropdown-toggle">
+						<i class="fa fa-user fa-fw"></i>人員管理
+					</a>
+					@if(Request::is('bureau/people','bureau/people/*'))
+					<ul class="list-unstyled collapse in" id="submenu2" aria-expanded="true">
+					@else
+                    <ul class="list-unstyled collapse" id="submenu2">
+					@endif
+                        <li {{ (Request::is('bureau/people') ? 'class=active' : '') }}><a class="func-item" href="{{ route('bureau.people') }}">瀏覽及搜尋</a></li>
+                        <li {{ (Request::is('bureau/people/new') ? 'class=active' : '') }}><a class="func-item" href="{{ route('bureau.createPeople') }}">新增人員</a></li>
+                        <li {{ (Request::is('bureau/people/json') ? 'class=active' : '') }}><a class="func-item" href="{{ route('bureau.jsonPeople') }}">匯入JSON</a></li>
+                    </ul>
+                </li>
+                <li {{ (Request::is('bureau/thirdapp') ? 'class=active' : '') }}>
+                    <a href="{{ route('bureau.thirdapp') }}"><i class="glyphicon glyphicon-asterisk" style="margin: 0 2px;"></i>第三方應用管理</a>
+                </li>
+                <li {{ (Request::is('bureau/OauthScopeAccessLog') ? 'class=active' : '') }}>
+                    <a href="{{ route('bureau.OauthScopeAccessLog') }}"><i class="fa fa-list-alt fa-fw"></i>使用者授權同意日誌查詢</a>
+                </li>
+                <li {{ (Request::is('bureau/usagerecord') ? 'class=active' : '') }}>
+                    <a href="{{ route('bureau.usagerecord') }}"><i class="fa fa-list-alt fa-fw"></i>系統作業日誌查詢</a>
+                </li>
+            </ul>
+        </nav>
 @stop
-
