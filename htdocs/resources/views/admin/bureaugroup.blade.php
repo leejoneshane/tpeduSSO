5c6f7975e9dfcec197df28acf8f28a42fcaff1e2
LEOSYS0827 <ldapsso0827@gmail.com>
2019-10-22 22:26:33 +0800

    單筆新增的區塊移至彈出框呈現

--- htdocs/resources/views/admin/bureaugroup.blade.php
+++ htdocs/resources/views/admin/bureaugroup.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.superboard')
 
 @section('page_heading')
-動態群組管理
+<h1 class="page-header">動態群組管理</h1>
 @endsection
 
 @section('section')
@@ -17,98 +17,100 @@
 		{{ session('success') }}
 	    </div>
 	@endif
-	<div class="col-sm-8">
-		<div class="panel panel-default">	  
-		<div class="panel-heading">
-			<h4>動態群組一覽表</h4>
+
+		<div class="panel-default">
+			<div class="panel-heading">
+				<a href="#addGroupModal" class="btn btn-success" data-toggle="modal" style="float: right;margin-top: 3px;">
+					<i class="glyphicon glyphicon-plus-sign"></i><span style="margin-left: 4px;">新增</span>
+				</a>
+				<h4>動態群組一覽表</h4>
+			</div>
+			<div class="panel-body">
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th style="min-width: 55px;">群組代號</th>
+							<th>過濾條件</th>
+							<th width="30%">管理</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($groups as $group)
+						<tr>
+							<form role="form" id="form" method="POST" action="{{ route('bureau.updateGroup', [ 'cn' => $group->cn ]) }}">
+							@csrf
+							<td style="vertical-align: inherit;">
+								<input id="cn" type="text" class="form-control" name="cn" value="{{ $group->cn ? $group->cn : old('cn') }}">
+							</td>
+							<td style="vertical-align: inherit;">
+								<span style="word-break: break-all;">{{ $group->url }}</span>
+							</td>
+							<td style="vertical-align: inherit;width: 200px">
+								<button type="button" style="margin-bottom: 4px;" class="btn btn-success" onclick="$('#deleteform').attr('action','{{ route('bureau.showMember', [ 'cn' => $group->cn ]) }}');$('#deleteform').submit();">成員</button>
+								<button type="button" style="margin-bottom: 4px;" class="btn btn-primary" onclick="$('#form').submit();">改名</button>
+								<button type="button" style="margin-bottom: 4px;" class="btn btn-danger" onclick="if(confirm('確定要刪除群組『'+$(this).parent().parent().find(':text').val()+'』?')){$('#deleteform').attr('action','{{ route('bureau.removeGroup', [ 'cn' => $group->cn ]) }}');$('#deleteform').submit();}">刪除</button>
+							</td>
+							</form>
+						</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
 		</div>
-		<div class="panel-body">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-						<th>群組代號</th>
-						<th>過濾條件</th>
-						<th>管理</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach ($groups as $group)
-					<tr>
-						<form role="form" id="form" method="POST" action="{{ route('bureau.updateGroup', [ 'cn' => $group->cn ]) }}">
-		    			@csrf
-						<td style="vertical-align: inherit;">
-							<input id="cn" type="text" class="form-control" name="cn" value="{{ $group->cn ? $group->cn : old('cn') }}">
-						</td>
-						<td style="vertical-align: inherit;">
-							<span>{{ $group->url }}</span>
-						</td>
-						<td style="vertical-align: inherit;width: 200px">
-							<button type="button" class="btn btn-success"
-							 	onclick="$('#deleteform').attr('action','{{ route('bureau.showMember', [ 'cn' => $group->cn ]) }}');
-										 $('#deleteform').submit();">成員</button>
-							<button type="button" class="btn btn-primary"
-							 	onclick="$('#form').submit();">改名</button>
-							<button type="button" class="btn btn-danger"
-							 	onclick="$('#deleteform').attr('action','{{ route('bureau.removeGroup', [ 'cn' => $group->cn ]) }}');
-										 $('#deleteform').submit();">刪除</button>
-						</td>
-						</form>
-					</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</div>
-		</div>
+
+		<form id="deleteform" action="" method="POST" style="display: none;">
+		@csrf
+		</form>
 	</div>
 
-    <form id="deleteform" action="" method="POST" style="display: none;">
-    @csrf
-    </form>
-    
-	<div class="col-sm-4">
-		<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4>新增動態群組</h4>
+	<div id="addGroupModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>新增動態群組</h4>
+					</div>
+					<div class="panel-body">
+						<form role="form" method="POST" action="{{ route('bureau.createGroup') }}">
+							@csrf
+							<div class="form-group{{ $errors->has('new-grp') ? ' has-error' : '' }}">
+								<label>群組代號</label>
+								<input id="new-grp" type="text" class="form-control" name="new-grp" value="{{ $errors->has('new-grp') ? old('new-grp') : '' }}"  placeholder="請輸入英文字母及數字" required>
+								@if ($errors->has('new-grp'))
+									<p class="help-block">
+										<strong>{{ $errors->first('new-grp') }}</strong>
+									</p>
+								@endif
+							</div>
+							<div class="form-group{{ $errors->has('model') ? ' has-error' : '' }}">
+								<label>群組模型</label>
+								<select id="model" class="form-control" name="model">
+								@foreach ($model as $m => $mdesc)
+									<option value="{{ $m }}">{{ $mdesc }}</option>
+								@endforeach
+								</select>
+							</div>
+							<div class="form-group{{ $errors->has('field') ? ' has-error' : '' }}">
+								<label style="display: block;">過濾條件</label>
+								(<select style="height: 34px;" id="field" name="field">
+								@foreach ($fields as $f => $fdesc)
+									<option value="{{ $f }}">{{ $fdesc }}</option>
+								@endforeach
+								</select>=<input style="height: 34px;" id="perform" type="text" name="perform" value="{{ $errors->has('perform') ? old('perform') : '' }}">)
+							</div>
+							<div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
+								<label>自訂過濾條件</label>
+								<input class="form-control" id="url" type="text" name="url" value="{{ $errors->has('url') ? old('url') : '' }}" placeholder="範例 ldap:///ou=people,dc=tp,dc=edu,dc=tw?mail?sub?(displayName=李*)">
+							</div>
+							<div class="form-group">
+								<button type="submit" class="btn btn-success">新增</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="panel-body">
-			<form role="form" method="POST" action="{{ route('bureau.createGroup') }}">
-		    	@csrf
-			    <div class="form-group{{ $errors->has('new-grp') ? ' has-error' : '' }}">
-					<label>群組代號</label>
-					<input id="new-grp" type="text" class="form-control" name="new-grp" value="{{ $errors->has('new-grp') ? old('new-grp') : '' }}"  placeholder="請輸入英文字母及數字" required>
-					@if ($errors->has('new-grp'))
-						<p class="help-block">
-							<strong>{{ $errors->first('new-grp') }}</strong>
-						</p>
-					@endif
-				</div>
-			    <div class="form-group{{ $errors->has('model') ? ' has-error' : '' }}">
-					<label>群組模型</label>
-					<select id="model" class="form-control" name="model">
-					@foreach ($model as $m => $mdesc)
-			    		<option value="{{ $m }}">{{ $mdesc }}</option>
-					@endforeach
-					</select>
-				</div>
-			    <div class="form-group{{ $errors->has('field') ? ' has-error' : '' }}">
-					<label style="display: block;">過濾條件</label>
-					(<select style="height: 34px;" id="field" name="field">
-					@foreach ($fields as $f => $fdesc)
-			    		<option value="{{ $f }}">{{ $fdesc }}</option>
-					@endforeach
-					</select>=<input style="height: 34px;" id="perform" type="text" name="perform" value="{{ $errors->has('perform') ? old('perform') : '' }}">)
-				</div>
-			    <div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
-					<label>自訂過濾條件</label>
-					<input class="form-control" id="url" type="text" name="url" value="{{ $errors->has('url') ? old('url') : '' }}" placeholder="範例 ldap:///ou=people,dc=tp,dc=edu,dc=tw?mail?sub?(displayName=李*)">
-				</div>
-			    <div class="form-group">
-					<button type="submit" class="btn btn-success">新增</button>
-				</div>
-			</form>
-		</div>
-		</div>
-	</div>    
 	</div>
 </div>
 @endsection
--- htdocs/resources/views/admin/schoolsubject.blade.php
+++ htdocs/resources/views/admin/schoolsubject.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('page_heading')
-教學科目管理
+<h1 class="page-header">教學科目管理</h1>
 @endsection
 
 @section('section')
@@ -17,101 +17,110 @@
 		{{ session('success') }}
 	    </div>
 	@endif
-	<div class="col-sm-6">
-		<div class="panel panel-default">	  
-		<div class="panel-heading">
-			<h4>教學科目一覽表</h4>
-		</div>
-		<div class="panel-body">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-						<th>科目代號</th>
-						<th>教學領域</th>
-						<th>科目名稱</th>
-						<th>管理</th>
-					</tr>
-				</thead>
-				<tbody>
+
+			<div class="panel panel-default">	  
+				<div class="panel-heading">
+					<a href="#addSubjectModal" class="btn btn-success" data-toggle="modal" style="float: right;margin-top: 3px;">
+						<i class="glyphicon glyphicon-plus-sign"></i><span style="margin-left: 4px;">新增</span>
+					</a>
+					<h4>教學科目一覽表</h4>
+				</div>
+				<div class="panel-body">
+					<table class="table table-hover" style="margin: 0;">
+						<thead>
+							<tr>
+								<th width="23%">科目代號</th>
+								<th width="23%">教學領域</th>
+								<th width="31%">科目名稱</th>
+								<th width="23%">管理</th>
+							</tr>
+						</thead>
+					</table>
 					@if (!empty($subjs))
 					@foreach ($subjs as $subj)
-					<tr>
 						<form role="form" method="POST" action="{{ route('school.updateSubject', [ 'dc' => $dc, 'subject' => $subj['tpSubject'] ]) }}">
-		    			@csrf
-						<td style="vertical-align: inherit;">
-							<label>{{ $subj['tpSubject'] }}</label>
-						</td>
-						<td>
-							<select class="form-control" name="domain">
-							@foreach ($domains as $domain)
-			    				<option value="{{ $domain }}"{{ array_key_exists('tpSubjectDomain', $subj) && $subj['tpSubjectDomain'] == $domain ? ' selected' : '' }}>{{ $domain }}</option>
-							@endforeach
-							</select>
-						</td>
-						<td>
-							<input id="description" type="text" class="form-control" name="description" value="{{ $subj['description'] ? $subj['description'] : old('description') }}">
-						</td>
-						<td>
-							<button type="submit" class="btn btn-primary">修改</button>
-							<button type="button" class="btn btn-danger"
-							 	onclick="$('#remove-form').attr('action','{{ route('school.removeSubject', [ 'dc' => $dc, 'subject' => $subj['tpSubject'] ]) }}');
-										 $('#remove-form').submit();">刪除</button>
-						</td>
+						@csrf
+						<table class="table table-hover" style="margin: 0;">
+							<tr>
+								<td width="23%" style="vertical-align: inherit;">
+									<label>{{ $subj['tpSubject'] }}</label>
+								</td>
+								<td width="23%">
+									<select class="form-control" name="domain">
+									@foreach ($domains as $domain)
+										<option value="{{ $domain }}"{{ array_key_exists('tpSubjectDomain', $subj) && $subj['tpSubjectDomain'] == $domain ? ' selected' : '' }}>{{ $domain }}</option>
+									@endforeach
+									</select>
+								</td>
+								<td width="31%">
+									<input id="description" type="text" class="form-control" name="description" value="{{ $subj['description'] ? $subj['description'] : old('description') }}">
+								</td>
+								<td width="23%">
+									<button type="submit" class="btn btn-primary">修改</button>
+									<button type="button" class="btn btn-danger"
+										onclick="if(confirm('確定要刪除『'+$(this).parent().parent().find(':text').val()+'』?')){$('#remove-form').attr('action','{{ route('school.removeSubject', [ 'dc' => $dc, 'subject' => $subj['tpSubject'] ]) }}');
+												 $('#remove-form').submit();}">刪除</button>
+								</td>
+							</tr>
+						</table>
 						</form>
-					</tr>
 					@endforeach
 					@endif
-				</tbody>
-			</table>
-		</div>
-		</div>
-	</div>
+				</div>
+			</div>
     
-    <form id="remove-form" action="" method="POST" style="display: none;">
-    @csrf
-    </form>
-
-	<div class="col-sm-6">
-		<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4>新增科目</h4>
-		</div>
-		<div class="panel-body">
-			<form role="form" method="POST" action="{{ route('school.createSubject', [ 'dc' => $dc ]) }}">
-		    	@csrf
-			    <div class="form-group{{ $errors->has('new-subj') ? ' has-error' : '' }}">
-					<label>科目代號</label>
-					<input id="new-subj" type="text" class="form-control" name="new-subj" value="{{ $errors->has('new-subj') ? old('new-subj') : '' }}" required>
-					@if ($errors->has('new-subj'))
-						<p class="help-block">
-							<strong>{{ $errors->first('new-subj') }}</strong>
-						</p>
-					@endif
-				</div>
-			    <div class="form-group">
-					<label>教學領域</label>
-					<select class="form-control" name="new-dom">
-					@foreach ($domains as $domain)
-			    		<option value="{{ $domain }}">{{ $domain }}</option>
-					@endforeach
-					</select>
-				</div>
-			    <div class="form-group{{ $errors->has('new-desc') ? ' has-error' : '' }}">
-					<label>科目名稱</label>
-					<input id="new-desc" type="text" class="form-control" name="new-desc" value="{{ $errors->has('new-desc') ? old('new-desc') : '' }}" required>
-					@if ($errors->has('new-desc'))
-						<p class="help-block">
-							<strong>{{ $errors->first('new-desc') }}</strong>
-						</p>
-					@endif
-				</div>
-			    <div class="form-group">
-					<button type="submit" class="btn btn-success">新增</button>
-				</div>
-			</form>
-		</div>
-		</div>
+		<form id="remove-form" action="" method="POST" style="display: none;">
+		@csrf
+		</form>
 	</div>
+
+	<div id="addSubjectModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="panel-default">
+					<div class="panel-heading">
+						<button type="button" data-dismiss="modal" aria-label="Close" class="close" style="padding: 8px 0;">
+							<i class="glyphicon glyphicon-remove" aria-hidden="true"></i>
+						</button>
+						<h4>新增科目</h4>
+					</div>
+					<div class="panel-body">
+						<form role="form" method="POST" action="{{ route('school.createSubject', [ 'dc' => $dc ]) }}">
+							@csrf
+							<div class="form-group{{ $errors->has('new-subj') ? ' has-error' : '' }}">
+								<label>科目代號</label>
+								<input id="new-subj" type="text" class="form-control" name="new-subj" value="{{ $errors->has('new-subj') ? old('new-subj') : '' }}" required>
+								@if ($errors->has('new-subj'))
+									<p class="help-block">
+										<strong>{{ $errors->first('new-subj') }}</strong>
+									</p>
+								@endif
+							</div>
+							<div class="form-group">
+								<label>教學領域</label>
+								<select class="form-control" name="new-dom">
+								@foreach ($domains as $domain)
+									<option value="{{ $domain }}">{{ $domain }}</option>
+								@endforeach
+								</select>
+							</div>
+							<div class="form-group{{ $errors->has('new-desc') ? ' has-error' : '' }}">
+								<label>科目名稱</label>
+								<input id="new-desc" type="text" class="form-control" name="new-desc" value="{{ $errors->has('new-desc') ? old('new-desc') : '' }}" required>
+								@if ($errors->has('new-desc'))
+									<p class="help-block">
+										<strong>{{ $errors->first('new-desc') }}</strong>
+									</p>
+								@endif
+							</div>
+							<div class="form-group">
+								<button type="submit" class="btn btn-success">新增</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 @endsection
