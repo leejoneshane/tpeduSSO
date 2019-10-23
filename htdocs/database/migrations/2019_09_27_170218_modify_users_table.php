d8eda78977514faf40fcb03a922ae17a40dd5b0b
李忠憲 <sean@florade-MacBook.local>
2019-10-23 22:24:58 +0800

    修改 users 資料表

--- htdocs/database/migrations/2019_09_27_170218_modify_users_table.php
+++ htdocs/database/migrations/2019_09_27_170218_modify_users_table.php
@@ -10,13 +10,13 @@ class ModifyUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */
-   
-	public function up() {
-		Schema::table('users', function (Blueprint $table) {
-			$table->timestamp('gsuite_created_at')->nullable();
-		});
-	}
+     */   
+    public function up() {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('gsuite_email')->nullable()->after('is_admin');
+            $table->timestamp('gsuite_created_at')->nullable()->after('gsuite_email');
+        });
+    }
 
     /**
      * Reverse the migrations.
@@ -25,6 +25,8 @@ class ModifyUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColume(['gsuite_email', 'gsuite_created_at']);
+        });
     }
 }
