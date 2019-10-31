ce7eb2cd0c7ffcdda6a0e4c26eb23a8d2a8b5590
leejoneshane <leejoneshane@gmail.com>
2019-10-31 15:52:24 +0800

    修復 oauth 金鑰管理頁面

---
+++ htdocs/app/Http/Controllers/oauthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class oauthController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+    public function index()
+    {
+        return view('oauthManager');
+    }
+}
