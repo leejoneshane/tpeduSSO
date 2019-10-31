657bfd23f220114c242a45c608c9765f94bafbde
leejoneshane <leejoneshane@gmail.com>
2019-10-31 18:31:57 +0800

     修正 Notification 命名

--- htdocs/app/Notifications/LockoutAdminNotification.php
+++ htdocs/app/Notifications/LockoutAdminNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class LockoutNotification extends Notification implements ShouldQueue
+class LockoutAdminNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     private $idno;
