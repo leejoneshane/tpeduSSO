fb594a674e8c6a9135cda91e40671bafd3037d2d
leejoneshane <leejoneshane@gmail.com>
2019-11-05 08:05:12 +0800

    fix bug

--- htdocs/app/Http/Middleware/RedirectIfAuthenticated.php
+++ htdocs/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,7 +20,7 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::check() && isset($request['SAMLRequest']) && $user->primary_gmail()) {  
+        if (Auth::check() && isset($request['SAMLRequest']) && Auth::user()->primary_gmail()) {  
             $this->handleSamlLoginRequest($request);
         }
         if (Auth::guard($guard)->check()) {
