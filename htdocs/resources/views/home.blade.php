62579ffc0ffce24efe03a81d96ed69148d7f1f50
leejoneshane <leejoneshane@gmail.com>
2019-11-18 19:35:27 +0800

    show createGsuite button

--- htdocs/resources/views/home.blade.php
+++ htdocs/resources/views/home.blade.php
@@ -19,7 +19,7 @@
                     注意：您使用的預設帳號在您畢業（或離職）後會自動刪除，請務必修改為自訂帳號才能長久保留使用！<br>
                     　　　同時，G-Suite 帳號將在啟用自訂帳號後才能啟用！
                     @elseif ($create_gsuite)
-                    要把您現在使用的帳號設定成您的 G-Suite 帳號嗎？<br>
+                    您要把現在使用的帳號同步到 G-Suite嗎？ <a href="{{ route('createGsuite') }}" class="btn btn-info">立即同步</a><br>
                     設定完成後，將可以使用 {{$gsuite}} 收發電子郵件或使用其它 G-Suite 服務。<br>
                     <ul>
                         <li>郵箱網址為 <a href="https://mail.google.com/a/ms.tp.edu.tw">https://mail.google.com/a/ms.tp.edu.tw</a></li>
