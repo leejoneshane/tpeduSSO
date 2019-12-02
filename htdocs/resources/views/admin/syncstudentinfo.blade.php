443cc5230e9cfcc3a3b5ffbe28732fd8da0553da
LEOSYS0827 <ldapsso0827@gmail.com>
2019-12-02 18:30:26 +0800

    文字及樣式調整

--- htdocs/resources/views/admin/syncstudentinfo.blade.php
+++ htdocs/resources/views/admin/syncstudentinfo.blade.php
@@ -42,17 +42,17 @@
 						<input type="checkbox" id="all" name="all" value="all">全部班級
 					</div>
 					<div class="form-group">
-						<select class="form-control" style="width:10%;display:inline" id="grade" name="grade">
+						<select class="form-control" style="width:auto;max-width: 100%;display:inline" id="grade" name="grade">
 							<option value="">同步年級</option>
 						@if (!empty($grades))
 						@foreach ($grades as $grade)
 							<option value="{{ $grade }}">{{ $grade }}年級</option>
 						@endforeach
 						@endif
-						</select>所有班級
+						</select><span style="margin-left: 7px;">所有班級</span>
 					</div>
 					<div class="form-group">
-						<select class="form-control" style="width:20%;display:inline" id="class" name="class">
+						<select class="form-control" style="width:auto;max-width: 100%;display:inline" id="class" name="class">
 							<option value="">同步班級</option>
 						@if (!empty($classes))
 						@foreach ($classes as $cls)
