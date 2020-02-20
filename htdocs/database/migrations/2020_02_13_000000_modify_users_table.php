2e3c22d9108563f87526387dbfc8a61c35a9e8c0
leejoneshane <leejoneshane@gmail.com>
2020-02-20 15:32:00 +0800

    update

--- htdocs/database/migrations/2020_02_13_000000_modify_users_table.php
+++ htdocs/database/migrations/2020_02_13_000000_modify_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class ModifyUsersTable extends Migration
 {
     /**
      * Run the migrations.
