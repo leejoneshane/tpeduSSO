80e2f34758de1772cf52675043055ee056c936ee
leejoneshane <leejoneshane@gmail.com>
2020-02-28 08:23:00 +0800

    修改介面

--- htdocs/resources/views/auth/socialiteManager.blade.php
+++ htdocs/resources/views/auth/socialiteManager.blade.php
@@ -20,7 +20,7 @@
                 <p>每種社群平台只能綁定一個帳號，若要變更已經綁定的社群帳號，請先解除綁定後再重新設定！</p>
                 @if ($google)
                     <div class="col-md-8">
-                        已綁定 Google 帳號：{{ $google->userID }}。
+                        Google 帳號：{{ $google->userID }}
                         <button type="button" class="btn btn-danger"
 							 	onclick="$('#form').attr('action','{{ route('socialite.remove') }}');
                                          $('#form').attr('method', 'POST');
@@ -30,13 +30,13 @@
                     </div>
                 @else
                     <div class="col-md-8">
-                        請綁定 Google 帳號：
-                        <a href="{{ route('login.google') }}"><img src="{{ asset('img/google.png') }}" style="border:0;width:50px;height:50px;"></a>
+                        Google 帳號：
+                        <a href="{{ route('login.google') }}" class="btn btn-primary">綁定</a>
                     </div>
                 @endif
                 @if ($facebook)
                     <div class="col-md-8">
-                        已綁定 Facebook 帳號：{{ $facebook->userID }}。
+                        Facebook 帳號：{{ $facebook->userID }}
                         <button type="button" class="btn btn-danger"
 							 	onclick="$('#form').attr('action','{{ route('socialite.remove') }}');
                                          $('#form').attr('method', 'POST');
@@ -46,13 +46,13 @@
                     </div>
                 @else
                     <div class="col-md-8">
-                        請綁定 Facebook 帳號：
-                        <a href="{{ route('login.facebook') }}"><img src="{{ asset('img/facebook.png') }}" style="border:0;width:50px;height:50px;"></a>
+                        Facebook 帳號：
+                        <a href="{{ route('login.facebook') }}" class="btn btn-primary">綁定</a>
                     </div>
                 @endif
                 @if ($yahoo)
                     <div class="col-md-8">
-                        已綁定 Yahoo 帳號：{{ $yahoo->userID }}。
+                        Yahoo 帳號：{{ $yahoo->userID }}
                         <button type="button" class="btn btn-danger"
 							 	onclick="$('#form').attr('action','{{ route('socialite.remove') }}');
                                          $('#form').attr('method', 'POST');
@@ -62,8 +62,8 @@
                     </div>
                 @else
                     <div class="col-md-8">
-                        請綁定 Yahoo 帳號：
-                        <a href="{{ route('login.yahoo') }}"><img src="{{ asset('img/yahoo.png') }}" style="border:0;width:50px;height:50px;"></a>
+                        Yahoo 帳號：
+                        <a href="{{ route('login.yahoo') }}" class="btn btn-primary">綁定</a>
                     </div>
                 @endif
                 <form id="form" action="" method="" style="display: none;">
