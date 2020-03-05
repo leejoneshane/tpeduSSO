3165755b72c11a042f6931ddecdffc854b7a8b21
leejoneshane <leejoneshane@gmail.com>
2020-03-05 16:02:02 +0800

    patch bugs

--- htdocs/app/Listeners/SendLockoutNotification.php
+++ htdocs/app/Listeners/SendLockoutNotification.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Providers\LdapServiceProvider;
 use App\Notifications\LockoutNotification;
+use App\Notifications\LockoutAdminNotification;
 
 class SendLockoutNotification
 {
