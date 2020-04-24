da78d61ac1782f07698ca5be82547194ad360616
leejoneshane <leejoneshane@gmail.com>
2020-04-24 18:47:34 +0800

    change logo

--- htdocs/resources/views/auth/login.blade.php
+++ htdocs/resources/views/auth/login.blade.php
@@ -4,9 +4,8 @@
 <div class="row">
     <div class="col-sm-8 col-sm-offset-2 text shadow">
         <img src="{{ asset('img/TaipeiEduLogo.png') }}" class="img-fluid logo" alt="Responsive image">
-        <h1><strong><font style="color:white">臺北市政府教育局</font></strong></h1>
         <div class="description">
-    	    <p><strong><font style="color:white;font-size:24pt;">單一身分驗證服務</font></strong></p>
+    	    <p><strong><font style="color:white;font-size:24pt;">{{ config('app.name') }}</font></strong></p>
     	</div>
     </div>
 </div>
